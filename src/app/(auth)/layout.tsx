89a18946c3b7088fdbbe603adf27fed6808466f2
Ayan <kumarayanatwork@gmail.com>
2025-07-28 14:36:56 +0530

    building the auth client

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,7 +6,6 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="bg-muted flex flex-col min-h-svh items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">{children}</div>
-      video till 1 18 28
     </div>
   );
 };
